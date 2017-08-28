3530c84283a845b8322412580d114ecdd7b18564
soom <dokgi1988@greedeat.com>
2017-08-28 15:32:46 +0900

    Initial Commit

---
+++ GitTagTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTagTest
+//
+//  Created by soom on 2017. 8. 28..
+//  Copyright © 2017년 greedeat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTagTestTests/GitTagTestTests.m
@@ -0,0 +1,39 @@
+//
+//  GitTagTestTests.m
+//  GitTagTestTests
+//
+//  Created by soom on 2017. 8. 28..
+//  Copyright © 2017년 greedeat. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GitTagTestTests : XCTestCase
+
+@end
+
+@implementation GitTagTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
